a4f2b3d2856039fd752f356422eee132d2f803ad
Luís Cláudio G. Rocha <luisclaudiogouveiarocha@gmail.com>
2016-05-01 16:27:09 -0300

    show blank window

--- main.cpp
+++ main.cpp
@@ -1,9 +1,62 @@
-#include <iostream>
+#include <GL/glew.h>
+#include <glfw3.h>
 #include "object.h"
+#include <iostream>
+
+void load_geometry(std::string filepath)
+{
+
+}
 
 int main(int argc, char** args)
 {
-	Object o;
-	o.load("./capsule.obj");
+	//This will setup a new window and OpenGL context.
+	//GLFW or GLUT may be used for this kind of stuff.
+	glfwInit();
+
+	glfwWindowHint(GLFW_SAMPLES, 1); //Supersampling, 4 samples/pixel
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); //Make sure OpenGL
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2); //version is 3.0
+
+	//Something about which kind of OpenGL will run here
+	//This says to glfw that we're using only OpenGL core functions, not
+	//vendor-specific
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	//Create window and set it as the context
+	GLFWwindow *window = glfwCreateWindow(800, 600, "Project 1", NULL, NULL);
+	glfwMakeContextCurrent(window);
+
+	//Initialize GLEW. Don't what the fuck is this.
+	glewExperimental = true; //Where the fuck is this coming from?
+	glewInit();
+
+	//Get keyboard input
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	//Main loop
+	do
+	{
+		//Clear screen -> this function also clears stencil and depth buffer
+		//glClear( GL_COLOR_BUFFER_BIT );
+
+		//Install our shader, draw geometry
+		/*
+		glUseProgram(shader_program_id);
+		glBindVertexArray(vertexArrayID);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
+		glUseProgram(0); */
+
+		//Swap buffer and query events
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	} while(glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && 
+			!glfwWindowShouldClose(window));
+
+	//Drop GLFW device
+	glfwTerminate();
+
 	return 0;
 }
